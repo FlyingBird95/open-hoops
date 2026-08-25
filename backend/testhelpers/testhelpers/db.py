7c4b0331fa00db1a31281727d596cbd8751d4b25
patrick <patrickvogel@live.nl>
2026-08-25 09:23:28 +0200

    Fix

--- backend/testhelpers/testhelpers/db.py
+++ backend/testhelpers/testhelpers/db.py
@@ -2,7 +2,7 @@
 
 import os
 
-from open_hoops.core.database import Base, Database  # noqa: F401
+from open_hoops.core.database import Database
 
 TEST_DB_URL = os.environ.get(
     "OPEN_HOOPS_TEST_DATABASE_URL",
